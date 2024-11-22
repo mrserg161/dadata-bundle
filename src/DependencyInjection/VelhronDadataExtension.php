55b5bfb3bdf4f84d288379d984fc9b40a5847b3f
Malovichko Sergey <mrserg161@gmail.com>
2024-11-22 13:27:51 +0300

    fix: Исправлены deprecate для PHP 8.4 и Symfony 7.1

--- src/DependencyInjection/VelhronDadataExtension.php
+++ src/DependencyInjection/VelhronDadataExtension.php
@@ -7,7 +7,7 @@ namespace Velhron\DadataBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class VelhronDadataExtension extends Extension
 {
@@ -19,7 +19,7 @@ class VelhronDadataExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
 
         $container->setParameter('velhron_dadata.token', $config['token']);
